e80e93471ad135e96178adf110d7dd0d1087c52a
Fagner Alexandre Zulin <fagnerzulin11@gmail.com>
2020-07-27 19:08:22 -0300

    Abrir os links das midias sociais em uma nova aba

--- src/components/Footer/components/RedesSociais/index.js
+++ src/components/Footer/components/RedesSociais/index.js
@@ -4,13 +4,19 @@ import { FooterSocialMedias, LinkSocialMedias } from "./style";
 function RedesSociais() {
   return (
     <FooterSocialMedias>
-      <LinkSocialMedias href="https://github.com/fagnerzulin/">
+      <LinkSocialMedias target="_blank" href="https://github.com/fagnerzulin/">
         <i class="fab fa-github fa-2x"></i>
       </LinkSocialMedias>
-      <LinkSocialMedias href="https://www.linkedin.com/in/fagner-zulin/">
+      <LinkSocialMedias
+        target="_blank"
+        href="https://www.linkedin.com/in/fagner-zulin/"
+      >
         <i class="fab fa-linkedin fa-2x"></i>
       </LinkSocialMedias>
-      <LinkSocialMedias href="https://www.instagram.com/fagnerzulin/">
+      <LinkSocialMedias
+        target="_blank"
+        href="https://www.instagram.com/fagnerzulin/"
+      >
         <i class="fab fa-instagram fa-2x"></i>
       </LinkSocialMedias>
     </FooterSocialMedias>
